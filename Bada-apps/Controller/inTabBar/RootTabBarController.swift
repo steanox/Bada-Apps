9acd7b65327e89f4941ebf5c9c4ea17a29965ba0
Handy <handy.kwok@gmail.com>
2018-03-07 18:55:05 +0700

    update tab bar item and custom tint colour

--- Bada-apps/Controller/inTabBar/RootTabBarController.swift
+++ Bada-apps/Controller/inTabBar/RootTabBarController.swift
@@ -9,14 +9,11 @@
 import UIKit
 
 
-class RootTabBarController: UITabBarController {
+class RootTabBarController: UITabBarController, UITabBarControllerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
     }
-    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -25,6 +22,25 @@ class RootTabBarController: UITabBarController {
         self.tabBar.layer.borderColor = UIColor.clear.cgColor
         self.tabBar.clipsToBounds = true
         
+        self.tabBar.items?[0].image = #imageLiteral(resourceName: "attendanceTabBarItem")
+        self.tabBar.items?[1].image = #imageLiteral(resourceName: "historyTabBarItem")
+        self.tabBar.items?[2].image = #imageLiteral(resourceName: "newsTabBarItem")
+        
+        self.tabBar.tintColor = UIColor(rgb: Color.attendanceImageColor)
+        
+    }
+    
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        switch item.selectedImage {
+        case #imageLiteral(resourceName: "attendanceTabBarItem")?:
+            self.tabBar.tintColor = UIColor(rgb: Color.attendanceImageColor)
+        case #imageLiteral(resourceName: "historyTabBarItem")?:
+            self.tabBar.tintColor = UIColor(rgb: Color.historyImageColor)
+        case #imageLiteral(resourceName: "newsTabBarItem")?:
+            self.tabBar.tintColor = UIColor(rgb: Color.newsImageColor)
+        default:
+            self.tabBar.tintColor = UIColor.brown
+        }
     }
     
     
